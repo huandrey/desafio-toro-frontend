8448f614ffed1c4645eebd9fc2d1a4b1d73ab87d
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-28 20:12:47 -0300

    feat: add configs tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      primary: "#6130b3",
+      white: "#ffffff",
+      "gray-100": "#f1f5f9",
+      "gray-400": "#94a3b8",
+      "gray-500": "#64748b",
+      "gray-500": "#475569",
+      
+    },
     extend: {},
   },
   plugins: [],
