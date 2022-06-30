8d9cd736d599accb75a89cf871f4a712945e20a4
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-30 13:19:14 -0300

    feat: add file responsible for the components exports

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-cycle
+export { default as Header } from './Header';
+export { default as Head } from './Head';
+export { default as Drawer } from './Drawer';
+export { default as Field } from './Field';
+export { default as Button } from './Button';
+export { default as Table } from './Table';
