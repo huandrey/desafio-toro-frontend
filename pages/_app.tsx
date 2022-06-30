ba553cb8d29eae037484a660a0f995db870217da
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-30 13:18:24 -0300

    feat: add chakra provider into the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({
+  components: {
+    Table: {
+      variants: {
+        mytable: {
+          tr: {
+            _odd: {
+              background: '!important green.800',
+            },
+          },
+          td: {
+            _odd: {
+              background: '!important green.800',
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
