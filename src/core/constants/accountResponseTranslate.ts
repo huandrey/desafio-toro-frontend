3fcdce37b72bd2d0c883b9853545e96a9a1e3308
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-30 13:23:51 -0300

    feat: create constant

---
+++ src/core/constants/accountResponseTranslate.ts
@@ -0,0 +1,9 @@
+const accountPropsTranslate: any = {
+  bank: 'Banco',
+  branch: 'Agencia',
+  account_number: 'Conta',
+  cpf: 'Seu CPF',
+  balance: 'Saldo',
+};
+
+export default accountPropsTranslate;
