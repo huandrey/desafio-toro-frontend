d53355b5075d57cea1771ca0a058716c5aa95f5d
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-06-30 13:24:01 -0300

    feat: create constant

---
+++ src/core/constants/drawerConstants.ts
@@ -0,0 +1,12 @@
+const drawerConstants = [
+  'Bolsa de Valores',
+  'Plataformas Avançadas',
+  'br',
+  'Fundos de Investimentos',
+  'Renda fixa e tesouro',
+  ' Todos os Produtos',
+  'br',
+  'Sair',
+];
+
+export default drawerConstants;
