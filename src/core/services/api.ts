cb0310d53e3cea85a120b31261afcbcac1ffe803
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:29:32 -0300

    feat: add default token to a api

--- src/core/services/api.ts
+++ src/core/services/api.ts
@@ -1,21 +1,26 @@
 import axios, { AxiosResponse } from 'axios';
+import { parseCookies } from 'nookies';
 import { HttpResponse, HttpClient, HttpRequest } from './protocols/httpClient';
 
 class AxiosHttpClient implements HttpClient<any> {
+  // eslint-disable-next-line class-methods-use-this
   async request(data: HttpRequest): Promise<HttpResponse> {
     let axiosResponse: AxiosResponse;
+    const { 'nextauth.token': token } = parseCookies();
     try {
       axiosResponse = await axios.request({
         url: `${process.env.API_URL}${data.url}`,
         method: data.method,
         data: data.body,
-        headers: data.headers,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
     } catch (error) {
       axiosResponse = error.response;
     }
 
-    return axiosResponse.data;
+    return { ...axiosResponse.data, statusCode: axiosResponse.status };
   }
 }
 
