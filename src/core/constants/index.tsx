c60b5f06080fbf85f066535756e2ca6a07593863
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:23:48 -0300

    feat: add new import for a signUpBenefits

--- src/core/constants/index.tsx
+++ src/core/constants/index.tsx
@@ -1,2 +1,3 @@
 export { default as drawerConstants } from './drawerConstants';
 export { default as accountResponseTranslate } from './accountResponseTranslate';
+export { default as signUpBenefits } from './signUpBenefits';
