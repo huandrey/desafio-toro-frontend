11e2a9c6ffa27eca7856be8301bb2b5033c1f53c
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:21:17 -0300

    feat: add new imports to a signin and signup

--- src/app/features/index.ts
+++ src/app/features/index.ts
@@ -1,2 +1,4 @@
 export { default as AccountList } from './AccountList';
 export { default as Header } from './Header';
+export { default as SignInForm } from './SignInForm';
+export { default as SignUpForm } from './SignUpForm';
