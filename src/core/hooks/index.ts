830b04a896abcd8cdef5ada895b401ed6dc509f6
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:26:47 -0300

    feat: add new imports for a hooks

---
+++ src/core/hooks/index.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-cycle
+export { default as useForms } from './useForms';
+export { default as useValidation } from './useValidation';
+export { default as useErrors } from './useErrors';
