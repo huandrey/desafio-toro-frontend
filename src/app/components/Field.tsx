bdff2b5abbc5392994c930e4f16c136158dce425
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:18:44 -0300

    feat: modified field structure

--- src/app/components/Field.tsx
+++ src/app/components/Field.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 
 function Field({
-  id, label, name, register, required, validation, error, ...inputProps
+  id,
+  label,
+  name,
+  value,
+  error,
+  placeholder,
+  type,
+  onChange,
+  autoComplete,
+  maxLength = 20,
+  ...inputProps
 }: any) {
   return (
     <div className="w-full flex flex-col gap-2 my-4">
-      <label htmlFor={name} className="block text-sm font-medium">{label}</label>
+      <label htmlFor={name} className="block text-sm font-medium">
+        {label}
+      </label>
       <div>
         <input
-          id={id}
-          ref={register}
+          id={label}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          type={type || 'text'}
+          onChange={onChange}
+          autoComplete={autoComplete}
           className="w-full px-4 py-2 rounded bg-gray-100"
-          {...register(name, { ...validation })}
+          maxLength={maxLength}
           {...inputProps}
         />
         <div className="h-0.5 bg-gray-400" />
-        <p className="py-2 text-xs text-red-600 font-bold">{error && error.message}</p>
+        <p className="py-2 text-xs text-red-600 font-bold">
+          {error}
+        </p>
       </div>
     </div>
   );
