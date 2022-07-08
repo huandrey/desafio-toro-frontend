44915febc0986e9252a24735f9782f9937208a10
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:28:46 -0300

    feat: add new service to a signup and recoveryUserData

--- src/core/services/auth.service.ts
+++ src/core/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { makeAxiosHttpClient } from './api';
+import { UserDTO } from './protocols/UserDTO';
 
 export const Auth = {
   signIn: async (body: any) => makeAxiosHttpClient().request({
     url: '/session',
     method: 'post',
     body,
+  }),
+  signUp: async (body: UserDTO) => makeAxiosHttpClient().request({
+    url: '/users',
+    method: 'post',
+    body,
+  }),
+  recoveryUserData: async () => makeAxiosHttpClient().request({
+    url: '/users/recovery-data',
+    method: 'post',
   }),
 };
