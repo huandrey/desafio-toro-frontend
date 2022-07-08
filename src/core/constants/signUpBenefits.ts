a459b1ef8c70e81e3e0afbf38177a7f71de6f66f
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:24:35 -0300

    feat: create signupbenefits constant

---
+++ src/core/constants/signUpBenefits.ts
@@ -0,0 +1,7 @@
+const signUpBenefits = [
+  'Corretagem Zero para investir em qualquer ativo: ações, minicontratos, FIIs, ETFs e mais.',
+  'Receba Cashback investindo em Fundos de Investimento.',
+  'Somos autorizados pelo Banco Central e já ajudamos mais de R$ 100 Bilhões a serem movimentados.',
+];
+
+export default signUpBenefits;
