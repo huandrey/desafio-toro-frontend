c9e8ccb4bb3ea802587c9749a841d6f1637b62bc
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-07 22:19:58 -0300

    feat: add new imports to submit and toastcontainer

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,3 +4,5 @@ export { default as Drawer } from './Drawer';
 export { default as Field } from './Field';
 export { default as Button } from './Button';
 export { default as Table } from './Table';
+export { default as ToastContainer } from './ToastContainer';
+export { default as Submit } from './Submit';
